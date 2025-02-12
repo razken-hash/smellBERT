b468e8ec81fc3b3335cdb7696958c86f69e0013e
razken-hash <ka_kenniche@esi.dz>
2025-02-12 14:04:19 +0100

    GithubUtils Added: generate_raw_url AND clone_code

---
+++ datasets/utils/github_utils.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class GithubUtils:
+
+    @staticmethod
+    def generate_raw_url(owner=None, repository=None, owner_and_repository=None, branch=None, commit_hash=None, path=None):
+        raw_url_base = "https://raw.githubusercontent.com"
+        if not owner_and_repository:
+            if not commit_hash:
+                return f"{raw_url_base}/{owner}/{repository}/{branch}{path}"
+            return f"{raw_url_base}/{owner}/{repository}/{commit_hash}{path}"
+        if not commit_hash:
+            return f"{raw_url_base}/{owner_and_repository}/{branch}{path}"
+        return f"{raw_url_base}/{owner_and_repository}/{commit_hash}{path}"
+
+    @staticmethod
+    def clone_code(raw_url=""):
+        try:
+            response = requests.get(raw_url)
+            if response.status_code == 200:
+                print(f"{response.status_code}: SUCCESS Code Clone")
+                return response.text
+            else:
+                print(f"{response.status_code}: FAILED Code Clone")
+        except:
+            print("EXCEPTION: FAILED Code Clone")
+        return ""
