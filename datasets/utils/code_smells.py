f24a6c54d829699746d37201667e7f155b57a723
razken-hash <ka_kenniche@esi.dz>
2025-02-19 01:52:19 +0100

    Tech Debt Database Explored :: Intermediate Datasets Generated :: FIXME: github_url not correct

--- datasets/utils/code_smells.py
+++ datasets/utils/code_smells.py
@@ -2,8 +2,17 @@
 
 class CodeSmells:
 
-    Blob = "Blob"
-    isBlob = "isBlob"
+    GodClass = "GodClass"
+    isGodClass = "isGodClass"
 
     DataClass = "DataClass"
     isDataClass = "isDataClass"
+
+    LazyClass = "LazyClass"
+    isLazyClass = "isLazyClass"
+
+    RefusedBequest = "RefusedBequest"
+    isRefusedBequest = "isRefusedBequest"
+
+    SpeculativeGenerality = "SpeculativeGenerality"
+    isSpeculativeGenerality = "isSpeculativeGenerality"
